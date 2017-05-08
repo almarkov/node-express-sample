4bfd4adf8fa42caeb6d0091f435fff8e429ea512
almarkov <almarkov89@gmail.com>
2017-05-08 18:01:46 +0300

    log rotation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var FileStreamRotator = require('file-stream-rotator')
+
+var fs = require('fs')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,6 +15,15 @@ var app = express();
 var server    = require('http').Server(app);
 var expressWs = require('express-ws')(app, server);
 
+var logDirectory = path.join(__dirname, 'log')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+var accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false
+})
+app.use(logger('combined', {stream: accessLogStream}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +31,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
