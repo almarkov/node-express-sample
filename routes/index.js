1178f55f49a577645eff9ee7311154f665993c5c
almarkov <almarkov89@gmail.com>
2019-11-23 21:46:33 +0300

    login page

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' })
 })
 
+router.get( '/login', (req, res, next) => {
+
+    var user = req.session.user || {}
+
+    res.render( 'login', {
+        title: 'Login',
+    })
+
+})
+
 module.exports = router
